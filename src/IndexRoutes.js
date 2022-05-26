d0cb580de5161ace48d30ac503e410983b2cdbd0
유 병재 <ybj1217@midasin.com>
2022-05-26 13:50:06 +0900

    ADD setting,package-install

---
+++ src/IndexRoutes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+const IndexRoutes = () => {
+    return (
+        <Routes>
+            <Route>
+            </Route>
+        </Routes>
+    );
+}
+ 
+export default IndexRoutes;
