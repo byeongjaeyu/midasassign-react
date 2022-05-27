1b82341718828c724772178e380c92b5dc0cbd98
유 병재 <ybj1217@midasin.com>
2022-05-27 14:17:06 +0900

    FIX news-menu DOM접근

--- src/Components/NewsMenu.jsx
+++ src/Components/NewsMenu.jsx
@@ -3,38 +3,35 @@ import Container from "./Container";
 import "../css/NewsMenu.css";
 
 const NewsMenu = (props) => {
+  const navMenuInfo = 
+  [
+    ["공지사항","notice"],
+    ["언론 속 이디야","news"],
+    ["홍보영상","pr"],
+    ["수상내역","award"],
+  ]
+
   const [selected, setSelected] = useState(props.mode);
 
-  const activated = (e) => {
-    let prevLink = document.getElementById(selected);
-    if (selected !== e.currentTarget.id && prevLink) {
-      prevLink.classList.remove("onmenu");
-      setSelected(e.currentTarget.id);
-    }
-  };
-
   useEffect(() => {
-    let currentLink = document.getElementById(selected);
-    if (currentLink) {
-      currentLink.className += " onmenu";
-      props.setMode(selected);
-    }
+    props.setMode(selected);
   }, [selected]);
 
   return (
     <ul className="menu">
-      <li className="menu-item" id="notice" onClick={activated}>
-        공지사항
-      </li>
-      <li className="menu-item" id="news" onClick={activated}>
-        언론 속 이디야
-      </li>
-      <li className="menu-item" id="pr" onClick={activated}>
-        홍보영상
-      </li>
-      <li className="menu-item" id="award" onClick={activated}>
-        수상내역
-      </li>
+      {
+        navMenuInfo.map(([title,path],index)=>{
+          return (
+            <li
+              className={"menu-item" + (selected === path ? " onmenu" : "")}
+              id={path}
+              onClick={()=>setSelected(path)}
+            >
+              {title}
+            </li>
+          )
+        })
+      }
     </ul>
   );
 };
