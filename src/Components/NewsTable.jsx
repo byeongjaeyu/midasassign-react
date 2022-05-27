ffcc583b6dad0ec3ba874e1e9a9bab2921d438e2
유 병재 <ybj1217@midasin.com>
2022-05-27 18:02:42 +0900

    ADD server

--- src/Components/NewsTable.jsx
+++ src/Components/NewsTable.jsx
@@ -3,15 +3,16 @@ const NewsTable = ({articles}) => {
     return ( 
         <table class="news_container" id="news_container">
             <thead>
-                <tr class="news_column">
+                <tr className="news_column">
                     <th>순서</th>
                     <th>본문</th>
                     <th>날짜</th>
                 </tr>
             </thead>
-            <tbody class="news_container_body" id="news_container_body">
+            <tbody className="news_container_body" id="news_container_body">
                 {
                     articles.map((article)=>{
+                        console.log(article);
                         return (
                             <td>
                                 <th className="news-order">{article.sn}</th>
